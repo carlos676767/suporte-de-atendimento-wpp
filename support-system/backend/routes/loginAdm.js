70856e514c51a1773dc57cf137cf04e174a17ce5
carlos676767 <carlitobr730@gmail.com>
2024-06-28 21:51:28 -0700

    JWT VALIDE OK

--- support-system/backend/routes/loginAdm.js
+++ support-system/backend/routes/loginAdm.js
@@ -4,36 +4,30 @@ const dbAuthAdmin = require("../db/adminLogin");
 const SECRET_KEY = cryopteor.randomBytes(32).toString("hex");
 require('dotenv').config({ path: '/app.env' })
 const bodyParser = require('body-parser');
-
+const cors = require("cors")
 
 module.exports = (api) => {
+  api.use(cors())
   api.use(bodyParser.json())
-  api.use(async(req,res,next) => {
-    const db = await dbAuthAdmin()
-    const {usuario, senha} =  req.body
-      verificarInput(usuario, senha,req)
-      if (usuario == db.usuario && senha == db.senha) {
-        const jwt = jsonWebToken.sign({usuario, senha}, SECRET_KEY, {expiresIn: "1h"})
-        res.send({status: 200, msg: "login feito com sucesso.", login: true, token: jwt})
-
-        next()
-      }else{
-        res.status(404).send({status: 404, login: false, msg: "Dados incorretos, verifique as credenciais."})
-      }
-  })
-
-  api.post("/loginAdm", async(res,req) => {
+  api.post("/loginAdm", async(req, res) => {
     try {
-      console.log('bem vindo');
-     await req.sendFile(__dirname, "F:\suporte de atendimento wpp\support-system\backend\index.html ")
+      const {usuario, senha} =  req.body
+      const db = await dbAuthAdmin()
+        if (usuario == db.usuario && senha == db.senha) {
+          const jwt = jsonWebToken.sign({usuario, senha}, SECRET_KEY, {expiresIn: "1h"})
+          res.send({status: 200, msg: "login feito com sucesso.", login: true, token: jwt})
+        }else{
+          res.status(404).send({status: 404, login: false, msg: "Dados incorretos, verifique as credenciais."})
+        }
     } catch (error) {
-    await  req.send({status: 404, login: false, msg: "ocorreu um erro tente novamente."}).status(404)  
+      console.log(error);
+      req.send({status: 404, login: false, msg: "ocorreu um erro tente novamente."}).status(404)  
     }
   });
 };
 
- async function verificarInput(usuario, senha, req) {
+  function verificarInput(usuario, senha, res) {
   if (!usuario && !senha) {
-    req.send({ msg: "Os dados estao vazios", login: false, status: 401 }).status(401);
+    res.send({ msg: "Os dados estao vazios", login: false, status: 401 }).status(401);
   }
 }
--- support-system/frontend/login/login.js
+++ support-system/frontend/login/login.js
@@ -3,6 +3,7 @@
 const inputUserName = document.getElementById("username");
 const passwordInput = document.getElementById("password");
 const button = document.querySelector("button");
+
 async function httpLogin() {
 const dados = {  usuario: inputUserName.value,  senha: passwordInput.value   }
   try {
@@ -14,12 +15,20 @@ const dados = {  usuario: inputUserName.value,  senha: passwordInput.value   }
         body: JSON.stringify(dados)
     })
     const dados2 = await httpRequest.json()
+    const {login} = dados2
+    valide(login)
     console.log(dados2);
   } catch (error) {
     console.log("ocorreu um erro inesperado", error);
   }
 }
 
+function valide(login) {
+  if (login == true) {
+    alert("em 5 segundos voce ira para a pagina.");
+    location.href = "../ticket.html";
+  }
+}
 
 button.addEventListener("click", () => {
     httpLogin()
